397e90a7cd290210ef78a908ae7b850f6540cfce
jfocha <jfocha@gmail.com>
2021-06-27 23:01:23 -0700

    it makes files now!

--- index.js
+++ index.js
@@ -24,7 +24,26 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+          // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+          if (err) {
+            reject(err);
+            // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+            return;
+          }
+    
+          // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+          resolve({
+            ok: true,
+            message: 'File created!'
+          });
+        });
+      });
+    };
+
 
 // TODO: Create a function to initialize app
 function init() {
@@ -183,8 +202,19 @@ function init() {
                 'The Unlicense'
             ],
         }
-    ]);
+    ]).then((answers) => {
+        // Use user feedback for... whatever!!
+        writeToFile('.README.md', generateMarkdown(answers))
+      })
+      .catch((error) => {
+        if (error.isTtyError) {
+          console.log("Prompt couldn't be rendered in the current environment");
+        } else {
+            console.log("Something else went wrong");
+        }
+      });
 }
+
 
 // Function call to initialize app
 init();
